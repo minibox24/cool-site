1918db058605ecc999ba29afe60d0619ce87bd96
minibox24 <minibox724@gmail.com>
2022-01-13 01:04:13 +0900

    기본 글꼴

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext } from 'next/document';
+
+export default class extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
