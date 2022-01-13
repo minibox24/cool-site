599ef004f2cab35cd5b1ad4232b08fa25cbf7f67
minibox24 <minibox724@gmail.com>
2022-01-13 22:33:08 +0900

    함수명 변경

--- pages/api/channels/[id]/index.js
+++ pages/api/channels/[id]/index.js
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { getChannel } from "../../../../utils/channel";
+import { checkChannel } from "../../../../utils/channel";
 
 const prisma = new PrismaClient();
 
 export default async function callback(req, res) {
-  const channel = await getChannel(req, res, prisma);
+  const channel = await checkChannel(req, res, prisma);
   if (!channel) return;
 
   res.status(200).json(channel);
--- utils/channel.js
+++ utils/channel.js
@@ -1,4 +1,4 @@
-export async function getChannel(req, res, prisma) {
+export async function checkChannel(req, res, prisma) {
   const channel = await prisma.channel.findUnique({ where: { id: req.query.id } });
 
   if (!channel) res.status(404).json({ error: "CHANNEL_NOT_FOUND" });
