f9a87f436a73562628e0a0f3dd9e836f927cc701
minibox24 <minibox724@gmail.com>
2022-01-13 17:16:09 +0900

    불필요 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import style from '../styles/home.module.css';
-
 export default function CoolHome() {
   return (
     <div className="grid place-items-center h-screen">
-      <span className="text-9xl font-black">멋진 사이트</span>
+      <div>
+        <span className="text-9xl font-black">멋진 사이트</span>
+      </div>
     </div>
   );
 }
