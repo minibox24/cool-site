cd1fb0741acc160355521ab073747f7e3af7d946
minibox24 <minibox724@gmail.com>
2022-01-13 17:10:54 +0900

    tailwindcss 사용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import style from '../styles/home.module.css';
 
 export default function CoolHome() {
-  return <span className={style.title}>멋진 사이트</span>;
+  return (
+    <div className="grid place-items-center h-screen">
+      <span className="text-9xl font-black">멋진 사이트</span>
+    </div>
+  );
 }
