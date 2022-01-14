e141b970235d12049c2d751fc4013e3baf51f847
minibox24 <minibox724@gmail.com>
2022-01-15 01:15:21 +0900

    타입스크립트

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-screen-md min-h-screen mx-auto ">
       <div>{children}</div>
--- utils/channel.js
+++
@@ -1,7 +0,0 @@
-export async function checkChannel(req, res, prisma) {
-  const channel = await prisma.channel.findUnique({ where: { id: req.query.id } });
-
-  if (!channel) res.status(404).json({ error: "CHANNEL_NOT_FOUND" });
-
-  return channel;
-}
